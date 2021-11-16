cbc3e90d48bb465ab2e108a5dbbbfb2d97706ac8
Bassy_Teisen <cab022111@coderacademy.edu.au>
2021-11-16 21:32:02 +1000

    Improved profiles and boats views

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,5 @@
 class ProfilesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :check_valid 
   before_action :set_profile, only: [:new, :edit, :create]
 
