04fc27304304ed9c5f96fe911e070a13bb0d57aa
Bassy_Teisen <cab022111@coderacademy.edu.au>
2021-11-16 22:02:48 +1000

    Voyage views and controllers are working also have validation

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -6,9 +6,9 @@ class Profile < ApplicationRecord
 
   has_one_attached :profile_image
   
-    validates :name, presence: {message: "Need to add a Name"}
-    validates :phone_number, presence: {message: "Need to add a Phone Number"}
-    validates :description, presence: {message: "Need to add a Description"}
+  validates :name, presence: {message: "Need to add a Name"}
+  validates :phone_number, presence: {message: "Need to add a Phone Number"}
+  validates :description, presence: {message: "Need to add a Description"}
 
    
 end
--- app/models/voyage.rb
+++ app/models/voyage.rb
@@ -8,4 +8,8 @@ class Voyage < ApplicationRecord
 
     has_many :applied_voyages, dependent: :destroy
     has_many :appliers, through: :applied_voyages, source: :user
+
+    validates :description, presence: {message: "Need to add a Description"}
+    validates :voyage, presence: {message: "Need to add a Date"}
+    validates :launch, presence: {message: "Need to add a Time"}
 end
