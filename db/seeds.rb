87fddb8509e064d4afc966d19060fecde489ccff
Bassy_Teisen <cab022111@coderacademy.edu.au>
2021-11-26 00:42:45 +1000

    Added more images to the profiles

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,15 +12,15 @@ Profile.destroy_all
 User.destroy_all
 
 User.create!(email: "Garry@1.com", password: "123456")
-User.create!(email: "Bob@2.com", password: "123456")
-User.create!(email: "Russel@3.com", password: "123456")
-User.create!(email: "Steve@4.com", password: "123456")
+User.create!(email: "Malcom@2.com", password: "123456")
+User.create!(email: "Steve@3.com", password: "123456")
+User.create!(email: "Josh@4.com", password: "123456")
 User.create!(email: "Allen@0.com", password: "123456")
-Profile.create!(user: User.first, name: "Garry", phone_number: 12345678, description: "Boating enthusist",profile_image: {io: File.open(Rails.root.join("./app/assets/images/surfing_cat.jpg")), filename: "surfing_cat.jpg"})
-Profile.create!(user: User.second, name: "Bob", phone_number: 12345678, description: "Boating enthusist",profile_image: {io: File.open(Rails.root.join("./app/assets/images/surfing_cat.jpg")), filename: "surfing_cat.jpg"})
-Profile.create!(user: User.third, name: "Russel", phone_number: 12345678, description: "Boating enthusist",profile_image: {io: File.open(Rails.root.join("./app/assets/images/surfing_cat.jpg")), filename: "surfing_cat.jpg"})
-Profile.create!(user: User.fourth, name: "Steve", phone_number: 12345678, description: "Boating enthusist",profile_image: {io: File.open(Rails.root.join("./app/assets/images/surfing_cat.jpg")), filename: "surfing_cat.jpg"})
-Profile.create!(user: User.fourth, name: "Allen", phone_number: 12345678, description: "Boating enthusist",profile_image: {io: File.open(Rails.root.join("./app/assets/images/surfing_cat.jpg")), filename: "surfing_cat.jpg"})
+Profile.create!(user: User.first, name: "Garry", phone_number: 12345678, description: "Boating enthusist",profile_image: {io: File.open(Rails.root.join("./app/assets/images/captain.jpg")), filename: "captain.jpg"})
+Profile.create!(user: User.second, name: "Malcom", phone_number: 12345678, description: "Boating enthusist",profile_image: {io: File.open(Rails.root.join("./app/assets/images/malcom.jpg")), filename: "malcom.jpg"})
+Profile.create!(user: User.third, name: "Steve", phone_number: 12345678, description: "Boating enthusist",profile_image: {io: File.open(Rails.root.join("./app/assets/images/pose.jpg")), filename: "pose.jpg"})
+Profile.create!(user: User.fourth, name: "Josh", phone_number: 12345678, description: "Boating enthusist",profile_image: {io: File.open(Rails.root.join("./app/assets/images/sailor.jpg")), filename: "sailor.jpg"})
+Profile.create!(user: User.fourth, name: "Allen", phone_number: 12345678, description: "Boating enthusist",profile_image: {io: File.open(Rails.root.join("./app/assets/images/posh.jpg")), filename: "posh.jpg"})
 
 Boat.create!(profile: Profile.first, category: "Tinny", size: "big", capasity: "120people", activity: "fishing",boat_image: {io: File.open(Rails.root.join("./app/assets/images/timber.jpg")), filename: "timber.jpg"})
 Boat.create!(profile: Profile.second, category: "Kayak", size: "big", capasity: "120people", activity: "fishing",boat_image: {io: File.open(Rails.root.join("./app/assets/images/smfishing.jpg")), filename: "smfishing.jpg"})
